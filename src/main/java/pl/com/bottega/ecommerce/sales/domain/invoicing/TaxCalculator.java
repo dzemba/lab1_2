5d3805e6ed8ac5dbc2eba9372c08e6fc78fb8cae
Rafal dzierzbinski 180157 <dzemba@op.pl>
2015-04-16 22:05:34 +0200

    refaktoryzacja klasy bookkeeper (po raz 3), dodanie tresci klasie taxcalc, dodanie tresci interfejsowi taxcalculator

--- src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/TaxCalculator.java
+++ src/main/java/pl/com/bottega/ecommerce/sales/domain/invoicing/TaxCalculator.java
@@ -1,5 +1,8 @@
-package pl.com.bottega.ecommerce.sales.domain.invoicing;
-
-public class TaxCalculator {
-
-}
+ package pl.com.bottega.ecommerce.sales.domain.invoicing;
+ 
+ import pl.com.bottega.ecommerce.sales.domain.productscatalog.ProductType;
+ import pl.com.bottega.ecommerce.sharedkernel.Money;
+ 
+ public interface TaxCalculator {
+ 	public Tax  calculateTax(Money money, ProductType productType);
+ }
